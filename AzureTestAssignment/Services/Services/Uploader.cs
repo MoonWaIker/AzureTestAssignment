4cf92b53da97a8aa513357ac379c6b8c5f0ab32b
MoonWalker <muhinmihajlo40@gmail.com>
2023-09-14 18:31:43 +0300

    Fixed azure blob storage access

--- AzureTestAssignment/Services/Services/Uploader.cs
+++ AzureTestAssignment/Services/Services/Uploader.cs
@@ -1,16 +1,15 @@
-﻿using Azure.Identity;
-using Azure.Storage.Blobs;
+﻿using Azure.Storage.Blobs;
 using AzureTestAssignment.Services.Interfaces;
 
 namespace AzureTestAssignment.Services.Services
 {
     public class Uploader : IUploader
     {
-        private const string connectionString = "https://azuretestassignment.blob.core.windows.net/blobcontainer";
+        private const string connectionString = "https://azuretestassignment.blob.core.windows.net/blobcontainer?sp=racwdl&st=2023-09-14T15:29:55Z&se=2023-09-20T23:29:55Z&spr=https&sv=2022-11-02&sr=c&sig=U%2BYqSNzP%2FAYXdK%2Fbnw%2BkBbTz9OHORb6poj5IUZEmgx4%3D";
 
         public void Upload(IFormFile file)
         {
-            BlobContainerClient containerClient = new(new Uri(connectionString), new DefaultAzureCredential());
+            BlobContainerClient containerClient = new(new Uri(connectionString));
 
             BlobClient blobClient = containerClient.GetBlobClient(file.FileName);
 
