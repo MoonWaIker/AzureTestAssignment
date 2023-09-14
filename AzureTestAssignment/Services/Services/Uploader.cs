d707a26c93f91605e892be5d6e400985b7d6afd5
MoonWalker <muhinmihajlo40@gmail.com>
2023-09-14 21:00:23 +0300

    Added an azure function project

--- AzureTestAssignment/Services/Services/Uploader.cs
+++ AzureTestAssignment/Services/Services/Uploader.cs
@@ -5,6 +5,7 @@ namespace AzureTestAssignment.Services.Services
 {
     public class Uploader : IUploader
     {
+        // TODO Make Managed identity
         private const string connectionString = "https://azuretestassignment.blob.core.windows.net/blobcontainer?sp=racwdl&st=2023-09-14T15:29:55Z&se=2023-09-20T23:29:55Z&spr=https&sv=2022-11-02&sr=c&sig=U%2BYqSNzP%2FAYXdK%2Fbnw%2BkBbTz9OHORb6poj5IUZEmgx4%3D";
 
         public void Upload(IFormFile file)
